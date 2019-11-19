3d99bd2b9e0c31622f458c5f8c09b2d2d3055842
Vineet <s3734938@student.rmit.edu.au>
2019-11-20 00:57:06 +1100

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DatingApp.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Value> Values { get; set; }
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ DatingApp.API/Model/Value.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace DatingApp.API.Model
+{
+    public class Value
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Value()
+        {
+          // 
+        }
+    }
+}
